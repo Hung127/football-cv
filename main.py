2b77de632d9103f1fc1c532ef80027d00c796002
Hung127 <hunghungtt05@gmail.com>
2026-07-31 00:15:59 +0700

    test: updated basic main for testing

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from utils import read_video, save_video
 
 def main():
     # read video
+    print("READING...")
     video_path = "./data/input/08fd33_4.mp4"
     video_frames = read_video(video_path=video_path)
 
     tracker = Tracker("./models/best.pt")
 
-    frames_info = tracker.detect_frames(video_frames) # type: ignore
+    # frames_info = tracker.detect_frames(video_frames) # type: ignore
+    print("TRACKING...")
+    tracks = tracker.get_obj_tracks(video_frames, read_from_stub=True, stub_path="./stubs/track_stubs.pkl")
 
-    # print(video_frames[0].shape)
+    print("DRAWING...")
+    processed_frames = tracker.draw_annotation(video_frames, tracks)
 
     # save video
-    # saved_video_path = "./data/output/output_video.avi"
-    # save_video(video_frames, saved_video_path)
+    print("SAVING...")
+    saved_video_path = "./data/output/output_video.avi"
+    save_video(processed_frames, saved_video_path)
 
 if __name__ == '__main__':
     main()
